fe537534dbd791a7d39bfcd39a6708e1c75286e7
Wagid Ali Sheikh <59254451+wagid-sheikh@users.noreply.github.com>
2025-11-21 18:08:23 +0530

    Corrected revision number to fit 32 character limit.

--- alembic/versions/0015_add_run_metadata_to_repeat_customers.py
+++ alembic/versions/0015_add_run_metadata_to_repeat_customers.py
@@ -6,7 +6,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0015_add_run_metadata_to_repeat_customers"
+revision = "0015_run_metadata_repeat_cust"
 down_revision = "0014_add_run_metadata_columns"
 branch_labels = None
 depends_on = None
