f8839f35f528155a6a4fde9f30c1db6247ab0c5c
Wagid Ali Sheikh <59254451+wagid-sheikh@users.noreply.github.com>
2026-03-16 23:30:37 +0530

    Renamed

--- alembic/versions/0078_store_status_subject_templ.py
+++ alembic/versions/0078_store_status_subject_templ.py
@@ -6,7 +6,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0078_store_scope_status_subject_template"
+revision = "0078_store_status_subject_templ"
 down_revision = "0077_standardize_td_uc_bodytempl"
 branch_labels = None
 depends_on = None
